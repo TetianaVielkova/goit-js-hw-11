78ae5ab6fe9bc00fb2ab4aea558e28794f2adf08
TanyaVielkova <106912349+TetianaVielkova@users.noreply.github.com>
2022-11-20 17:42:27 +0300

    1

---
+++ src/fetchImages.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+async function fetchImages(name, page, perPage) {
+    
+    const API_KEY = '31367220-5a96f337331fc9bdf5943a9df';
+    const BASE_URL = 'https://pixabay.com/api/';
+
+    try {
+        const response = await axios.get(`${BASE_URL}?key=${API_KEY}&=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`)
+    return response.dsts
+    } catch (error) {
+        console.log('ERROR: ', error);
+    }
+}
+
+export { fetchImages };
--- src/index.js
+++ src/index.js
@@ -5,63 +5,72 @@ import axios from 'axios';
 
 const formRef = document.querySelector('#search-form');
 const galleryRef = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
 
+loadMoreBtn.addEventListener('click', onClickLoadMore);
 formRef.addEventListener('submit', onSearch);
+
+loadMoreBtn.style.display = 'none';
 
 const API_KEY = '31367220-5a96f337331fc9bdf5943a9df';
 const BASE_URL = 'https://pixabay.com/api/';
-counter = 1;
-let gallery = new SimpleLightbox('.gallery a', {
-    captions: true,
-    captionsData: 'alt',
-    captionDelay: 250,
-})
 
 const options = {
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: 'true',
-    page: 1,
     per_page: 40,
+    page: 1,
 };
 
+let lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
+let maxQuery = 0;
+
+function onClickLoadMore(){
+    loadMoreBtn.style.display = "none";
+    fetchPictures(fetchPictures.searchQuery)
+    .then(createGalleryItem);
+}
 function onSearch(e){
     e.preventDefault();
     galleryRef.innerHTML = '';
+    maxQuery = 0;
+    options.page = 1;
     fetchPictures.searchQuery = e.currentTarget.elements.searchQuery.value;
-    console.log(searchQuery);
-    startPage();
-    resetQuery();
     fetchPictures(fetchPictures.searchQuery)
-        .then(data => {
-            createGalleryItem(data);
-            Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
-        });
+    .then(data => {
+        createGalleryItem(data);
+        
+    });
 }
 
 async function fetchPictures(searchQuery){
     try {
-        
-        const response = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${searchQuery}`, { options },);
-        options.per_page += response.data.hits.length;
+        const response = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${searchQuery}`, { options } );
+        options.page +=1;
         return response.data;
     } catch(error) {
         console.log(error);
     }
 }
 
-
 function createGalleryItem(data) {
-    if (data.hits.length === 0) {
+
+    if ((searchQuery.length === 0)) {
         Notiflix.Notify.warning('Sorry, there are no images matching your search query. Please try again.');
         return
-    } else {
+    }
+    else {
     const markup = data.hits.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => {
         return `<div class="photo-card">
         <a href="${largeImageURL}">
             <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
         </a>
-        <div class="info">
         <div class="info">
             <p class="info-item">
                 <b>Likes</b>${likes}
@@ -75,31 +84,16 @@ function createGalleryItem(data) {
             <p class="info-item">
                 <b>Downloads</b>${downloads}
             </p>
-        </div>
         </div>`}).join("");
 
         galleryRef.insertAdjacentHTML("beforeend", markup);
-        gallery.refresh();
-
-        const { height: cardHeight } = document
-        .querySelector(".gallery")
-            .firstElementChild.getBoundingClientRect();
-
-        window.scrollBy({
-        top: cardHeight * 2,
-        behavior: "smooth",
-    });
-
-    if (data >= data.totalHits) {
-        Notiflix.Notify.warning(`We're sorry, but you've reached the end of search results.`);
-        return
-    }
-    }}
-
-
-    window.addEventListener('scroll', () => {
-        const {scrollHeight, scrollTop, clientHeight} = document.documentElement
-        if(scrollHeight - clientHeight === scrollTop){
-        createGalleryItem()
+        lightbox.refresh();
+        loadMoreBtn.style.display = "block";
+        maxQuery += 40;
+        if (maxQuery >= data.totalHits) {
+            Notiflix.Notify.warning(`We're sorry, but you've reached the end of search results.`);
+            loadMoreBtn.style.display = "none";
+            return
         }
-    })
+    };
+}
