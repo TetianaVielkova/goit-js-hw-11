57aa5d979af3dc041241738afc686b4c36c7e48b
TanyaVielkova <106912349+TetianaVielkova@users.noreply.github.com>
2022-11-21 13:45:45 +0300

    1

--- src/index.js
+++ src/index.js
@@ -57,24 +57,24 @@ function onSearch(e){
 
 function createGalleryItem(name) {
     const markup = name.hits.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => {
-        return `<div class="photo-card">
-        <a href="${largeImageURL}">
+        return `<a href="${largeImageURL}">
+        <div class="photo__card">
             <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
         </a>
         <div class="info">
-            <p class="info-item">
+            <p class="info__item">
                 <b>Likes</b>${likes}
             </p>
-            <p class="info-item">
+            <p class="info__item">
                 <b>Views</b>${views}
             </p>
-            <p class="info-item">
+            <p class="info__item">
                 <b>Comments</b>${comments}
             </p>
-            <p class="info-item">
+            <p class="info__item">
                 <b>Downloads</b>${downloads}
             </p>
-        </div>`}).join("");
+        </div></div>`}).join("");
 
         galleryRef.insertAdjacentHTML("beforeend", markup);
         lightbox.refresh();
